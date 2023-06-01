c9ccdd660910240ece65d6fb4590a166e676558f
TiHedze <tilen.hedzet@student.math.hr>
2023-06-01 17:50:35 +0200

    added search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthorComponent } from './author/author.component';
 import { PublicationComponent } from './publication/publication.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   {path:'authors', component: AuthorComponent},
-  {path: 'publications', component: PublicationComponent}
+  {path: 'publications', component: PublicationComponent},
+  {path:'search', component: SearchComponent}
 ];
 
 @NgModule({
